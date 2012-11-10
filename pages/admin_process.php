21b0658111439d00d6a3f35b5bf7305df120325f
Pranav Gandhi <pranavrulz@gmail.com>
2012-11-10 07:39:10 +0000

    Added entry to admin_process to add new users using lib.php addUser()

--- pages/admin_process.php
+++ pages/admin_process.php
@@ -1,8 +1,16 @@
 <?php
 
+# Edit an existing user
 if($_POST['type'] == "edit" && $_POST['name'] != NULL && $_POST['points'] != NULL){
   editUser($_POST['name'], $_POST['points'], $_POST['achievements']);
   header("Location:/admin/success");
+
+  # Add a new user
+} elseif($_POST['type'] == "add" && $_POST['name'] != NULL && $_POST['points'] != NULL){
+  addUser($_POST['name'], $_POST['points'], $_POST['achievements']);
+  header("Location:/admin/success");
+
+  # Send to 404 page
 } else
   header("Location:/404");
 
