c370ad78636f7123adb61b9b17d8927b242971eb
Pranav Gandhi <pranavrulz@gmail.com>
2012-11-10 06:29:58 +0000

    Added a quick n dirty user highlight on the home page

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,17 @@
-<?php include('header.php'); ?>
+<?php
 
+include('header.php');
 
+$users = loadUsers();
+foreach($users as $user){
+?>
+<div class="user">
+  <h2><?=$user->name?></h2>
+  <div class="progressbarouter"><div class="progressbarinner" style="width:<?=$user->points?>%;"></div></div>
+</div>
+<?php
+}
 
-<?php include('footer.php'); ?>
+include('footer.php');
+
+?>
