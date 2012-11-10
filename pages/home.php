b554f2e66aa43fd8d31fe677d4a35f50ebffe90f
Pranav Gandhi <pranavrulz@gmail.com>
2012-11-10 07:57:49 +0000

    Changed name field to a link of value name that sends to user profile page

--- pages/home.php
+++ pages/home.php
@@ -6,7 +6,7 @@ $users = loadUsers();
 foreach($users as $user){
 ?>
 <div class="user">
-  <h2><?=$user->name?></h2>
+  <h2><a href="/user/<?=$user->name?>"><?=$user->name?></a></h2>
   <div class="progressbarouter"><div class="progressbarinner" style="width:<?=$user->points?>%;"></div></div>
 </div>
 <?php
